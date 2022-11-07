c22b59035b5d58fc084373165fbfbe4e5dec7dcd
huntmori <huntmori04x22@gmail.com>
2022-11-08 00:33:56 +0900

    생활코딩 11.컴포넌트만들기(1)

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 import {Component} from "react";
+
+class Subject extends Component {
+    render() {
+        return (
+            <header>
+                <h1>WEB</h1>
+                World Wide Web!!
+            </header>
+        );
+    }
+}
+
+class Navigator extends Component {
+    render() {
+        return(
+            <nav>
+                <ul>
+                    <li>
+                        <a href="1.html">HTML</a>
+                    </li>
+                    <li>
+                        <a href="1.html">CSS</a>
+                    </li>
+                    <li>
+                        <a href="1.html">JavaScript</a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+class Article extends Component {
+    render() {
+        return (
+            <article>
+                <h2>HTML</h2>
+                HTML is HyperText Markup Language.
+            </article>
+        );
+    }
+}
 
 class App extends Component {
   render() {
     return (
         <div className="App">
-            Hello, React!!
+            <Subject></Subject>
+            <Navigator></Navigator>
+            <Article></Article>
         </div>
     );
   }
